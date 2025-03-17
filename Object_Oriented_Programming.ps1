0c95f03f20661862f485a7772ba8708ebeee443d
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2025-03-17 15:05:23 -0400

    Adding OOP
    This file helps demonstrate object oriented programming in powershell.

---
+++ Object_Oriented_Programming.ps1
@@ -0,0 +1,48 @@
+﻿class Laptop_Specs {
+    [String]$Brand
+    [Int]$Year
+    [Int]$RAM
+    [Int]$StorageGB
+    [Int]$RefreshRateHz
+    [String]$GraphicsCard
+    [String]$ScreenResolution
+    [String]$OperatingSystem
+    [Int]$CPUCores
+
+[String] Retailer([String]$StoreBrand)
+{
+return "Store was $StoreBrand"
+}
+
+[Float] RetailPrice([Float]$StorePrice)
+{
+return $StorePrice
+}
+
+[Float] RetailRating([Float]$StoreRating)
+{
+   return $StoreRating
+}
+
+}
+
+$laptopOne = New-Object Laptop_Specs
+$laptopOne.Brand = 'Dell'
+$laptopOne.Year = 2024
+$laptopOne.RAM = 8
+$laptopOne.StorageGB = 512
+$laptopOne.RefreshRateHZ = 60
+$laptopOne.GraphicsCard = 'Intel UHD Graphics'
+$laptopOne.ScreenResolution = '1920 X 1080'
+$laptopOne.OperatingSystem = 'Windows 11'
+$laptopOne.CPUCores = 10
+
+
+
+
+#Printing Object
+$laptopOne
+
+$laptopOne.Retailer('BestBuy')
+$laptopOne.RetailPrice(598.99)
+$laptopOne.RetailRating(4.5)
